d647b7d2e3f0a2cb31eb70949b37a55c07718c7f
Alicia Rosenthal <aliciarosenthal@gmail.com>
2013-09-22 19:02:42 -0300

    matriz de resultados correccion de estilos

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -5,28 +5,37 @@ package edu.tallerweb.pptls;
  */
 public class Mano {
 	private Forma forma;
+
 	/**
-	 * Toda Mano debe crearse con una forma dada, que será
-	 * la que determine su condición en el juego.
-	 * @param forma, la Forma que adopta la Mano.
+	 * Toda Mano debe crearse con una forma dada, que será la que determine su
+	 * condición en el juego.
+	 * 
+	 * @param forma
+	 *            , la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
 		this.forma = forma;
 	}
 
 	/**
-	 * Evaluará el resultado de la partida según las reglas
-	 * del juego.
-	 * @param otra, la otra Mano.
+	 * Evaluará el resultado de la partida según las reglas del juego.
+	 * 
+	 * @param otra
+	 *            , la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		Resultado tablaResultado[][] = {{Resultado.EMPATA,Resultado.PIERDE,Resultado.PIERDE,Resultado.GANA,Resultado.GANA},
-                {Resultado.GANA,Resultado.EMPATA,Resultado.PIERDE,Resultado.PIERDE,Resultado.GANA},
-                {Resultado.GANA,Resultado.GANA,Resultado.EMPATA,Resultado.PIERDE,Resultado.PIERDE},
-                {Resultado.PIERDE,Resultado.GANA,Resultado.GANA,Resultado.EMPATA,Resultado.PIERDE},
-                {Resultado.PIERDE,Resultado.PIERDE,Resultado.GANA,Resultado.GANA,Resultado.EMPATA}};
-
+		Resultado tablaResultado[][] = {
+				{ Resultado.EMPATA, Resultado.PIERDE, Resultado.PIERDE,
+						Resultado.GANA, Resultado.GANA },
+				{ Resultado.GANA, Resultado.EMPATA, Resultado.PIERDE,
+						Resultado.PIERDE, Resultado.GANA },
+				{ Resultado.GANA, Resultado.GANA, Resultado.EMPATA,
+						Resultado.PIERDE, Resultado.PIERDE },
+				{ Resultado.PIERDE, Resultado.GANA, Resultado.GANA,
+						Resultado.EMPATA, Resultado.PIERDE },
+				{ Resultado.PIERDE, Resultado.PIERDE, Resultado.GANA,
+						Resultado.GANA, Resultado.EMPATA } };
 		return tablaResultado[this.forma.getValor()][otra.forma.getValor()];
 	}
 
