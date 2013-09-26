ef8915a2880d02325861fce33faa9fd7fd1523c2
Alicia Rosenthal <aliciarosenthal@gmail.com>
2013-09-26 16:51:13 -0300

    partida opcion02 correccion de estilo

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -9,9 +9,7 @@ public class Mano {
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será la que determine su
 	 * condición en el juego.
-	 * 
-	 * @param forma
-	 *            , la Forma que adopta la Mano.
+	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
 		this.forma = forma;
@@ -19,14 +17,11 @@ public class Mano {
 
 	/**
 	 * Evaluará el resultado de la partida según las reglas del juego.
-	 * 
-	 * @param otra
-	 *            , la otra Mano.
+	 * @param otra, la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		if (this.forma.getValor() - otra.forma.getValor() == -3
-				|| this.forma.getValor() - otra.forma.getValor() == -4
+		if (this.forma.getValor() - otra.forma.getValor() <-2
 				|| this.forma.getValor() - otra.forma.getValor() == 2
 				|| this.forma.getValor() - otra.forma.getValor() == 1) {
 			return Resultado.GANA;
